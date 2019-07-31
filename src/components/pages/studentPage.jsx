500931de0d5a8cd15712b46b4ca7887347e15f41
Iulia <imi17@imperial.ac.uk>
2019-07-31 16:19:48 +0100

    Add name and shortcode fields

--- src/components/pages/studentPage.jsx
+++ src/components/pages/studentPage.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 import SurveyComponent from "../surveyComponent";
+import { Form, FormGroup, Label, Input, Col, Container } from "reactstrap";
+class Student extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-class StudentPage extends Component {
   render() {
     return (
       <div>
-        <h1> Student</h1>
+        <h1>Student survey</h1>
         <SurveyComponent />
       </div>
     );
   }
 }
 
-export default StudentPage;
+export default Student;
--- src/components/surveyComponent.jsx
+++ src/components/surveyComponent.jsx
@@ -1,16 +1,33 @@
 import React, { Component } from "react";
 import * as Survey from "survey-react";
 import "survey-react/survey.css";
+import { Form, FormGroup, Label, Input, Container, Col } from "reactstrap";
 
 class SurveyComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = { isCompleted: false };
+    this.state = {
+      isCompleted: false,
+      shortcode: "",
+      name: ""
+    };
     this.onCompleteComponent = this.onCompleteComponent.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleShortcodeChange = this.handleShortcodeChange.bind(this);
   }
 
   onCompleteComponent() {
     this.setState({ isCompleted: true });
+  }
+
+  handleNameChange(event) {
+    this.setState({ name: event.target.value });
+    console.log(this.state);
+  }
+
+  handleShortcodeChange(event) {
+    this.setState({ shortcode: event.target.value });
+    console.log(this.state);
   }
 
   render() {
@@ -61,16 +78,38 @@ class SurveyComponent extends Component {
     };
 
     var surveyRender = !this.state.isCompleted ? (
-      <Survey.Survey
-        json={json}
-        showCompletedPage={false}
-        onComplete={this.onCompleteComponent}
-      />
+      <div>
+        <Container>
+          <Form>
+            <FormGroup row>
+              <Label sm={1}> Name </Label>
+              <Col sm={5}>
+                <Input
+                  name="name"
+                  value={this.state.name}
+                  onChange={this.handleNameChange}
+                />
+              </Col>
+              <Label sm={1}> Shortcode </Label>
+              <Col sm={5}>
+                <Input
+                  name="shortcode"
+                  value={this.state.shortcode}
+                  onChange={this.handleShortcodeChange}
+                />
+              </Col>
+            </FormGroup>
+          </Form>
+        </Container>
+        <Survey.Survey
+          json={json}
+          showCompletedPage={false}
+          onComplete={this.onCompleteComponent}
+        />
+      </div>
     ) : null;
 
-    var onCompleteComponent = this.state.isCompleted ? (
-      <div>The component after onComplete event</div>
-    ) : null;
+    var onCompleteComponent = this.state.isCompleted ? <div>{}}</div> : null;
     return (
       <div>
         {surveyRender}
