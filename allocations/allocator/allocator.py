7bb7d9680bcdd6e47920450d2fe5e4c2e830c6d7
Iulia <imi17@imperial.ac.uk>
2019-08-06 19:09:46 +0100

    Try Mongoengine

--- allocations/allocator/allocator.py
+++ allocations/allocator/allocator.py
@@ -1,1 +1,11 @@
+from mongoengine import connect, fields, Document
 
+
+class Fresher(Document):
+    student = fields.DictField()
+    interests = fields.DictField()
+    self_description = fields.StringField()
+
+
+for fresher in Fresher.objects:
+    print(fresher)
