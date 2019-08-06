d080376432e5520e99c57c183915be55221f9489
Iulia <imi17@imperial.ac.uk>
2019-08-06 19:49:22 +0100

    Dummy allocations

--- allocations/allocator/allocator.py
+++ allocations/allocator/allocator.py
@@ -1,11 +1,29 @@
 from mongoengine import connect, fields, Document
 
+db = connect(db='demo', host='mongodb://localhost:27017')
 
 class Fresher(Document):
     student = fields.DictField()
     interests = fields.DictField()
-    self_description = fields.StringField()
+    selfDescription = fields.StringField()
+    meta = {'collection': 'fresher_models', 'strict': False}
 
 
-for fresher in Fresher.objects:
-    print(fresher)
+class Family(Document):
+    kids = fields.ListField(fields.ReferenceField(Fresher))
+
+
+def allocate_and_save(freshers):
+    kids = []
+
+    for fresher in freshers:
+        kids.append(fresher)
+        if len(kids) == 4:
+            family = Family(kids=kids)
+            family.save()
+            kids = []
+
+
+for family in Family.objects:
+    for kid in family.kids:
+        print(kid['student'])
