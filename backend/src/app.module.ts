6041f9a453f6f4e601e29feee6d237edb98bc753
Jack Pordi <jackel119@gmail.com>
2019-08-18 23:58:39 +0100

    passport wip

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SignupModule } from './signup/signup.module';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     SignupModule,
     TypegooseModule.forRoot(
       'mongodb://localhost/demo',
-    )],
+    ),
+    AuthModule],
 })
 export class ApplicationModule {}
