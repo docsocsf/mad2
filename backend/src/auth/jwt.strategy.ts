6b36b4a2b2366a1da0498412c466cf857b2177d3
Jack Pordi <jackel119@gmail.com>
2019-08-20 17:28:20 +0100

    Add back iat and expiration

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -13,7 +13,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    const {auth, membership, data, ...other} = payload;
-    return {auth, membership, data};
+    return payload;
   }
 }
