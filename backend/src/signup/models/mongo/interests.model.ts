4820dc4b53073fbe50504a91c5d2ae83134a82cd
Jack Pordi <jackel119@gmail.com>
2019-08-28 20:00:44 +0100

    Changed backend interests

--- backend/src/signup/models/mongo/interests.model.ts
+++ backend/src/signup/models/mongo/interests.model.ts
@@ -6,24 +6,81 @@ export class Interests extends Typegoose {
   alcohol: number;
 
   @prop({ min: 0, max: 5 })
-  clubbing: number;
-
-  @prop({ min: 0, max: 5 })
   anime: number;
 
   @prop({ min: 0, max: 5 })
-  sports: number;
+  artGraphics: number;
+
+  @prop({ min: 0, max: 5 })
+  baking: number;
+
+  @prop({ min: 0, max: 5 })
+  charity: number;
+
+  @prop({ min: 0, max: 5 })
+  clubbing: number;
 
   @prop({ min: 0, max: 5 })
   cooking: number;
 
   @prop({ min: 0, max: 5 })
-  performingMusic: number;
+  danceBallroom: number;
+
+  @prop({ min: 0, max: 5 })
+  danceContemporary: number;
+
+  @prop({ min: 0, max: 5 })
+  dramatics: number;
+
+  @prop({ min: 0, max: 5 })
+  exerciseAndHealth: number;
+
+  @prop({ min: 0, max: 5 })
+  film: number;
+
+  @prop({ min: 0, max: 5 })
+  finance: number;
+
+  @prop({ min: 0, max: 5 })
+  football: number;
+
+  @prop({ min: 0, max: 5 })
+  hiking: number;
 
   @prop({ min: 0, max: 5 })
   kpop: number;
 
   @prop({ min: 0, max: 5 })
-  dance: number;
+  martialArts: number;
+
+  @prop({ min: 0, max: 5 })
+  otherSports: number;
+
+  @prop({ min: 0, max: 5 })
+  performingMusicClassical: number;
+
+  @prop({ min: 0, max: 5 })
+  performingMusicPopRockJazz: number;
+
+  @prop({ min: 0, max: 5 })
+  photography: number;
+
+  @prop({ min: 0, max: 5 })
+  politics: number;
+
+  @prop({ min: 0, max: 5 })
+  racketSports: number;
+
+  @prop({ min: 0, max: 5 })
+  rowing: number;
+
+  @prop({ min: 0, max: 5 })
+  rugby: number;
+
+  @prop({ min: 0, max: 5 })
+  tabletopGames: number;
+
+  @prop({ min: 0, max: 5 })
+  videoGames: number;
 
 }
---
+++ frontend/src/components/Survey/FresherSurveyComponent.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import * as Survey from "survey-react";
+import "survey-react/survey.css";
+import { config, hobbies } from "./FresherSurveyConfig";
+import axios from 'axios';
+
+class SurveyComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    const defaultThemeColors = Survey.StylesManager.ThemeColors["default"];
+    defaultThemeColors["$main-color"] = "#225590";
+    defaultThemeColors["$main-hover-color"] = "#2255f9";
+    defaultThemeColors["$text-color"] = "#4a4a4a";
+    defaultThemeColors["$header-color"] = "#225590";
+
+    defaultThemeColors["$header-background-color"] = "#4a4a4a";
+    defaultThemeColors["$body-container-background-color"] = "#f8f8f8";
+
+    Survey.StylesManager.applyTheme();
+    this.state = {
+      isCompleted: false,
+      submissionSuccess: false,
+    };
+    this.onCompleteComponent = this.onCompleteComponent.bind(this);
+  }
+
+  async onCompleteComponent(results) {
+    this.setState({ isCompleted: true });
+    let response = await axios.post("/api/signup/fresher", 
+      this.serializeResults(results.data));
+    if (response.status === 201) {
+      this.setState({ submissionSuccess: true });
+    }
+  }
+
+  serializeResults(results) {
+    const student = {};
+    student.firstName = results.firstName.trim();
+    student.lastName = results.lastName.trim();
+    student.preferredName = results.preferredName ? results.preferredName.trim() : null;
+    student.shortcode = results.shortcode;
+
+    const interestsOutput = {};
+
+
+    for (let hobby of hobbies) {
+      let score;
+      if (results.interests) {
+        score = results.interests[hobby];
+      } else {
+        score = 0;
+      }
+      interestsOutput[hobby] = score !== undefined ? Number(score) : 0
+    }
+
+    return {
+      student: student,
+      interests: interestsOutput,
+      selfDescription: results.selfDescription ? results.selfDescription.trim() : null,
+    }
+  }
+
+  render () {
+    return (
+      <>
+        { !this.state.isCompleted &&
+          <Survey.Survey
+            json={config}
+            showCompletedPage={false}
+            onComplete={this.onCompleteComponent}
+          />
+        }
+
+        {
+          this.state.isCompleted && this.state.submissionSuccess &&
+            <h1>
+              Submission success!
+            </h1>
+        }
+
+        {
+          this.state.isCompleted && !this.state.submissionSuccess &&
+            <h1>
+              Submission Servey error, please try again later
+            </h1>
+        }
+      </>
+    );
+  }
+}
+
+export default SurveyComponent;
