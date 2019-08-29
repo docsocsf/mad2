d5c2e904521ef43968e7718a7172ecb5b02426f7
Jack Pordi <jackel119@gmail.com>
2019-08-29 23:14:55 +0100

    Frontned changes for proposal error handling

--- frontend/src/components/Parents/Proposals.jsx
+++ frontend/src/components/Parents/Proposals.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react';
+import axios from 'axios';
 import {
   Alert,
   Card,
@@ -10,15 +10,15 @@ import {
   FormGroup,
   Col,
   Row,
-  Label
-} from "reactstrap";
+  Label,
+} from 'reactstrap';
 
 class Proposals extends Component {
   constructor(props) {
     super(props);
     this.state = {
       error: false,
-      errorMessage: ""
+      errorMessage: '',
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -27,50 +27,48 @@ class Proposals extends Component {
     event.preventDefault();
     this.setState({
       error: false,
-      errorMessage: ""
+      errorMessage: '',
     });
     const username = event.target.username.value;
-    if (localStorage.shortcode === username) {
+    if (username === '') {
       this.setState({
         error: true,
-        errorMessage: "You cannot propose to yourself."
-      });
-    } else if (username === "") {
-      this.setState({
-        error: true,
-        errorMessage: "Shortcode cannot be empty"
+        errorMessage: 'Shortcode cannot be empty',
       });
     } else {
       try {
         await axios.post(
-          "/api/signup/parent/propose",
+          '/api/signup/parent/propose',
           {
-            partnerShortcode: username
+            partnerShortcode: username,
           },
           {
-            withCredentials: true
-          }
+            withCredentials: true,
+          },
         );
         this.props.getStatus();
       } catch (err) {
-        this.setState({
-          error: true,
-          errorMessage:
-            "The partner you have tried to propose to did not sign up yet. Please ask them to sign up and then try again."
-        });
+        this.setState({ error: true });
+        let errorMessage;
+        if (err.response) {
+          errorMessage = err.response.data.message;
+        } else {
+          errorMessage = 'Server is down right now, please try again later';
+        }
+        this.setState({ errorMessage });
       }
     }
   }
 
   async propose(username) {
     await axios.post(
-      "/api/signup/parent/propose",
+      '/api/signup/parent/propose',
       {
-        partnerShortcode: username
+        partnerShortcode: username,
       },
       {
-        withCredentials: true
-      }
+        withCredentials: true,
+      },
     );
     window.setTimeout(() => {
       this.props.getStatus();
@@ -78,68 +76,64 @@ class Proposals extends Component {
   }
 
   render() {
-    const to = this.props.to;
-    const from = this.props.from;
-    const error = this.state.error;
-    const errorMessage = this.state.errorMessage;
+    const { to } = this.props;
+    const { from } = this.props;
+    const { error } = this.state;
+    const { errorMessage } = this.state;
     return (
       <div>
         {to.length > 0 && (
-          <Card style={{ marginTop: "10px" }}>
+          <Card style={{ marginTop: '10px' }}>
             <CardHeader tag="h3">People that proposed to you</CardHeader>
             <CardBody>
-              {to.map(value => {
-                return (
-                  <Alert color={"primary"} key={value.proposeTs}>
-                    <Row>
-                      <Col sm={9}>
-                        {value.proposerId.student.firstName}{" "}
-                        {value.proposerId.student.lastName}
-                        {", "}
-                        {value.proposerId.student.shortcode}
-                      </Col>
-                      <Col sm={3}>
-                        <Button
-                          block
-                          outline
-                          color="primary"
-                          onClick={() =>
-                            this.propose(value.proposerId.student.shortcode)
-                          }
-                        >
+              {to.map((value) => (
+                <Alert color="primary" key={value.proposeTs}>
+                  <Row>
+                    <Col sm={9}>
+                      {value.proposerId.student.firstName}
+                      {' '}
+                      {value.proposerId.student.lastName}
+                      {', '}
+                      {value.proposerId.student.shortcode}
+                    </Col>
+                    <Col sm={3}>
+                      <Button
+                        block
+                        outline
+                        color="primary"
+                        onClick={() => this.propose(value.proposerId.student.shortcode)}
+                      >
                           Accept Proposal
-                        </Button>
-                      </Col>
-                    </Row>
-                  </Alert>
-                );
-              })}
+                      </Button>
+                    </Col>
+                  </Row>
+                </Alert>
+              ))}
             </CardBody>
           </Card>
         )}
         {from.length > 0 && (
-          <Card style={{ marginTop: "10px" }}>
+          <Card style={{ marginTop: '10px' }}>
             <CardHeader tag="h3">People you Proposed to</CardHeader>
             <CardBody>
-              {from.map(value => {
-                return (
-                  <Alert color={"primary"} key={value.proposeTs}>
-                    {value.proposeeId.student.firstName}{" "}
-                    {value.proposeeId.student.lastName}
-                    {", "}
-                    {value.proposeeId.student.shortcode}
-                  </Alert>
-                );
-              })}
+              {from.map((value) => (
+                <Alert color="primary" key={value.proposeTs}>
+                  {value.proposeeId.student.firstName}
+                  {' '}
+                  {value.proposeeId.student.lastName}
+                  {', '}
+                  {value.proposeeId.student.shortcode}
+                </Alert>
+              ))}
             </CardBody>
           </Card>
         )}
-        <Card style={{ marginTop: "10px" }}>
+        <Card style={{ marginTop: '10px' }}>
           <CardHeader tag="h3">Propose to someone</CardHeader>
           <CardBody>
-            {error && <Alert color={"danger"}>{errorMessage}</Alert>}
+            {error && <Alert color="danger">{errorMessage}</Alert>}
             <form onSubmit={this.handleSubmit}>
-              <FormGroup row controlid="email" style={{ paddingTop: "10px" }}>
+              <FormGroup row controlid="email" style={{ paddingTop: '10px' }}>
                 <Col>
                   <Label>Shortcode of partner</Label>
                   <Input
