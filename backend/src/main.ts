4e898832bce12d04692b81ce581773f2d04f77ff
Jack Pordi <jackel119@gmail.com>
2019-09-03 13:25:12 +0100

    improve logging

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,13 @@ import { ApplicationModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as morgan from 'morgan';
 import * as helmet from 'helmet';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(ApplicationModule);
   app.enableCors();
-  app.use(morgan('tiny'));
+  app.use(morgan('common', {stream: fs.createWriteStream('../../mad.log', {flags: 'a'})}));
+  app.use(morgan('dev'));
   app.use(helmet());
 
   await app.listen(8080);
