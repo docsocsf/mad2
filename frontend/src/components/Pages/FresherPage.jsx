bd62714cc77368b387aa8b1ee26c4c8bdab54f01
George Soteriou <george@pop-os.localdomain>
2019-09-08 21:05:21 +0100

    update signup message

--- frontend/src/components/Pages/FresherPage.jsx
+++ frontend/src/components/Pages/FresherPage.jsx
@@ -1,35 +1,20 @@
 import React, { Component } from "react";
 import { Col, Container, Row } from "reactstrap";
 import FresherSurveyComponent from "../Survey/FresherSurveyComponent";
-import isLoggedIn from "../Auth/utils";
-import Family from "./Family";
 
 class FresherPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      loggedIn: isLoggedIn()
-    };
   }
 
   render() {
-    const { loggedIn } = this.state;
-
     return (
       <div>
-        <h1
-          style={{
-            textAlign: "center"
-          }}
-        >
-          Student survey
-        </h1>
         <Container>
           <Row>
             <Col sm={1} />
             <Col sm={10}>
-              {!loggedIn && <FresherSurveyComponent />}
-              {loggedIn && Family}
+              <FresherSurveyComponent />
             </Col>
             <Col sm={1} />
           </Row>
--- frontend/src/components/Survey/FresherSurveyComponent.js
+++ frontend/src/components/Survey/FresherSurveyComponent.js
@@ -1,34 +1,37 @@
-import React, { Component } from 'react';
-import * as Survey from 'survey-react';
-import 'survey-react/survey.css';
-import axios from 'axios';
-import { config, hobbies } from './FresherSurveyConfig';
+import React, { Component } from "react";
+import * as Survey from "survey-react";
+import "survey-react/survey.css";
+import axios from "axios";
+import { config, hobbies } from "./FresherSurveyConfig";
 
 class SurveyComponent extends Component {
   constructor(props) {
     super(props);
 
     const defaultThemeColors = Survey.StylesManager.ThemeColors.default;
-    defaultThemeColors['$main-color'] = '#225590';
-    defaultThemeColors['$main-hover-color'] = '#2255f9';
-    defaultThemeColors['$text-color'] = '#4a4a4a';
-    defaultThemeColors['$header-color'] = '#225590';
+    defaultThemeColors["$main-color"] = "#225590";
+    defaultThemeColors["$main-hover-color"] = "#2255f9";
+    defaultThemeColors["$text-color"] = "#4a4a4a";
+    defaultThemeColors["$header-color"] = "#225590";
 
-    defaultThemeColors['$header-background-color'] = '#4a4a4a';
-    defaultThemeColors['$body-container-background-color'] = '#f8f8f8';
+    defaultThemeColors["$header-background-color"] = "#4a4a4a";
+    defaultThemeColors["$body-container-background-color"] = "#f8f8f8";
 
     Survey.StylesManager.applyTheme();
     this.state = {
       isCompleted: false,
       submissionSuccess: false,
+      email: ""
     };
     this.onCompleteComponent = this.onCompleteComponent.bind(this);
   }
 
   async onCompleteComponent(results) {
     this.setState({ isCompleted: true });
-    const response = await axios.post('/api/signup/fresher',
-      this.serializeResults(results.data));
+    const response = await axios.post(
+      "/api/signup/fresher",
+      this.serializeResults(results.data)
+    );
     if (response.status === 201) {
       this.setState({ submissionSuccess: true });
     }
@@ -38,14 +41,17 @@ class SurveyComponent extends Component {
     const student = {};
     student.firstName = results.firstName.trim();
     student.lastName = results.lastName.trim();
-    student.preferredName = results.preferredName ? results.preferredName.trim() : null;
+    student.preferredName = results.preferredName
+      ? results.preferredName.trim()
+      : null;
     student.gender = results.gender;
     student.course = results.course;
     student.shortcode = results.shortcode;
-    student.socialMedia = results.socialMedia ? results.socialMedia.trim() : null;
+    student.socialMedia = results.socialMedia
+      ? results.socialMedia.trim()
+      : null;
 
     const interestsOutput = {};
-
 
     for (const hobby of hobbies) {
       let score;
@@ -56,44 +62,50 @@ class SurveyComponent extends Component {
       }
       interestsOutput[hobby] = score !== undefined ? Number(score) : 0;
     }
-
+    this.setState({ email: student.shortcode + "@imperial.ac.uk" });
     return {
       student,
       interests: interestsOutput,
-      selfDescription: results.selfDescription ? results.selfDescription.trim() : null,
+      selfDescription: results.selfDescription
+        ? results.selfDescription.trim()
+        : null
     };
   }
 
   render() {
     return (
-      <>
-        { !this.state.isCompleted
-          && (
-          <Survey.Survey
-            json={config}
-            showCompletedPage={false}
-            onComplete={this.onCompleteComponent}
-          />
-          )}
-
-        {
-          this.state.isCompleted && this.state.submissionSuccess
-            && (
-            <h1>
-              Submission success!
+      <div style={{ textAlign: "center" }}>
+        {!this.state.isCompleted && (
+          <>
+            <h1
+              style={{
+                textAlign: "center"
+              }}
+            >
+              Student survey
             </h1>
-            )
-        }
+            <Survey.Survey
+              json={config}
+              showCompletedPage={false}
+              onComplete={this.onCompleteComponent}
+            />
+          </>
+        )}
 
-        {
-          this.state.isCompleted && !this.state.submissionSuccess
-            && (
-            <h1>
-              Submission Servey error, please try again later
-            </h1>
-            )
-        }
-      </>
+        {this.state.isCompleted && this.state.submissionSuccess && (
+          <>
+            <h1>Signup success!</h1>
+            <p>
+              Please verify your signup by clicking on the link we sent to you
+              at {this.state.email}
+            </p>
+          </>
+        )}
+
+        {this.state.isCompleted && !this.state.submissionSuccess && (
+          <h1>Submission Survey error, please try again later</h1>
+        )}
+      </div>
     );
   }
 }
