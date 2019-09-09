29c0a44501bf071f7e2c830f986096bda1fee991
Jack Pordi <jackel119@gmail.com>
2019-09-09 20:03:08 +0100

    Fix text align

--- frontend/src/components/Survey/FresherSurveyComponent.js
+++ frontend/src/components/Survey/FresherSurveyComponent.js
@@ -1,27 +1,27 @@
-import React, { Component } from "react";
-import * as Survey from "survey-react";
-import "survey-react/survey.css";
-import axios from "axios";
-import { config, hobbies } from "./FresherSurveyConfig";
+import React, { Component } from 'react';
+import * as Survey from 'survey-react';
+import 'survey-react/survey.css';
+import axios from 'axios';
+import { config, hobbies } from './FresherSurveyConfig';
 
 class SurveyComponent extends Component {
   constructor(props) {
     super(props);
 
     const defaultThemeColors = Survey.StylesManager.ThemeColors.default;
-    defaultThemeColors["$main-color"] = "#225590";
-    defaultThemeColors["$main-hover-color"] = "#2255f9";
-    defaultThemeColors["$text-color"] = "#4a4a4a";
-    defaultThemeColors["$header-color"] = "#225590";
+    defaultThemeColors['$main-color'] = '#225590';
+    defaultThemeColors['$main-hover-color'] = '#2255f9';
+    defaultThemeColors['$text-color'] = '#4a4a4a';
+    defaultThemeColors['$header-color'] = '#225590';
 
-    defaultThemeColors["$header-background-color"] = "#4a4a4a";
-    defaultThemeColors["$body-container-background-color"] = "#f8f8f8";
+    defaultThemeColors['$header-background-color'] = '#4a4a4a';
+    defaultThemeColors['$body-container-background-color'] = '#f8f8f8';
 
     Survey.StylesManager.applyTheme();
     this.state = {
       isCompleted: false,
       submissionSuccess: false,
-      email: ""
+      email: '',
     };
     this.onCompleteComponent = this.onCompleteComponent.bind(this);
   }
@@ -29,8 +29,8 @@ class SurveyComponent extends Component {
   async onCompleteComponent(results) {
     this.setState({ isCompleted: true });
     const response = await axios.post(
-      "/api/signup/fresher",
-      this.serializeResults(results.data)
+      '/api/signup/fresher',
+      this.serializeResults(results.data),
     );
     if (response.status === 201) {
       this.setState({ submissionSuccess: true });
@@ -62,24 +62,24 @@ class SurveyComponent extends Component {
       }
       interestsOutput[hobby] = score !== undefined ? Number(score) : 0;
     }
-    this.setState({ email: student.shortcode + "@imperial.ac.uk" });
+    this.setState({ email: `${student.shortcode}@imperial.ac.uk` });
     return {
       student,
       interests: interestsOutput,
       selfDescription: results.selfDescription
         ? results.selfDescription.trim()
-        : null
+        : null,
     };
   }
 
   render() {
     return (
-      <div style={{ textAlign: "center" }}>
+      <div style={{ textAlign: 'left' }}>
         {!this.state.isCompleted && (
           <>
             <h1
               style={{
-                textAlign: "center"
+                textAlign: 'center',
               }}
             >
               Student survey
@@ -97,7 +97,9 @@ class SurveyComponent extends Component {
             <h1>Signup success!</h1>
             <p>
               Please verify your signup by clicking on the link we sent to you
-              at {this.state.email}
+              at
+              {' '}
+              {this.state.email}
             </p>
           </>
         )}
